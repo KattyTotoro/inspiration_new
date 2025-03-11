9812696e7b46e819283aa522149c4c74a857e5df
KattyTotoro <89184036854@yandex.ru>
2025-03-11 19:30:40 +0300

    dfdf

--- pages/recipes.vue
+++ pages/recipes.vue
@@ -1,139 +1,26 @@
 <template>
 
-    <div class="content" style="display: flex;">
+    
+    
+    <section class="content">
 
-    <h1><strong>Живое питание.</strong></h1>
+    
+        <h1><strong>Живое питание.</strong></h1>
+       
+        <div class="blockList">
 
-        <section class="recipes">
-
-            <div class="recipe">
-
-
-                <img src="/img/recipesImg/5312294218217155704.jpg" width="200px">
-
-                <div>
-                   <h2><strong>СВЕКОЛЬНЫЙ ХУМУС</strong></h2> 
-
-<p>
-                    Ингредиенты:
-
-                    1️⃣ 1/2 стакана нута <br>
-                    2️⃣ 1 ст. л. тахини или кунжутного урбеча (если блендер мощный, то просто кунжутное семя)<br>
-                    3️⃣ 1 небольшая свекла<br>
-                    4️⃣ Сок 1/4 лимона<br>
-                    5️⃣ 3 ст.л. оливкового масла<br>
-                    6️⃣ Вода из под нута для нужной консистенции (50-100 мл)<br>
-                    7️⃣ Специи:<br>
-                    🟢 1-2 зубчика чеснока (можно запечь)<br>
-                    🟢1 ч.л. семян кумина (1/2 ч.л., если кумин молотый)<br>
-                    🟢1/2 ч.л. молотой красной паприки<br>
-                    🟢соль и черный перец по вкусу<br>
-                    🟢щепотка куркумы<br>
-                    🟢щепотка острого красного перца<br>
-                    🟢лавровый лист<br>
-                </p>
-
-                  <p>  Приготовление:
-
-                    ✔️Замочите нут на ночь, утром слейте воду, залейте новой и варите 30-40 минут с лавровым листом и
-                    черным перцем горошком
-                    ✔️Свеклу запеките в духовке (180 градусов 50-60 минут, до готовности)
-                    ✔️Почистите готовую свеклу
-                    ✔️Смешайте в блендере все ингредиенты
-                </p>
-
-                 <p>   ❤️ Подавать с хрустящими (нутовыми, кукурузными) лепешками и блюдами из круп
-
-                    @lera_ayurveda
-                    #ВдохПищи
-                    </p>
-                </div>
-
-
-            </div>
-
-        </section>
-
-        <section>
-            
-            <div class="blockList">
-
-                <div>
-                    <h2>Рецепт 8</h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-                    reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-                    quaerat sunt nam non dolores blanditiis.
-                    <br>
-                    <a href="#">Подробнее</a>
-                </div>
-
-                <div>
-                    <h2>Рецепт 7</h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-                    reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-                    quaerat sunt nam non dolores blanditiis.
-                    <br>
-                    <a href="#">Подробнее</a>
-                </div>
-
-                <div>
-                    <h2>Рецепт 6</h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-                    reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-                    quaerat sunt nam non dolores blanditiis.
-                    <br>
-                    <a href="#">Подробнее</a>
-                </div>
-
-                <div>
-                    <h2>Рецепт 5</h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-                    reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-                    quaerat sunt nam non dolores blanditiis.
-                    <br>
-                    <a href="#">Подробнее</a>
-                </div>
-
-                <div>
-                    <h2>Рецепт 4</h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-                    reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-                    quaerat sunt nam non dolores blanditiis.
-                    <br>
-                    <a href="#">Подробнее</a>
-                </div>
-
-                <div>
-                    <h2>Рецепт 3</h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-                    reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-                    quaerat sunt nam non dolores blanditiis.
-                    <br>
-                    <a href="#">Подробнее</a>
-                </div>
-
-                <div>
-                    <h2>Рецепт 2</h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-                    reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-                    quaerat sunt nam non dolores blanditiis.
-                    <br>
-                    <a href="#">Подробнее</a>
-                </div>
-
-                <div>
-                    <h2>Рецепт 1</h2>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam adipisci veniam error fugit nemo,
-                    reprehenderit consequatur eligendi molestiae beatae similique amet officia quidem recusandae,
-                    quaerat sunt nam non dolores blanditiis.
-                    <br>
-                    <a href="#">Подробнее</a>
+            <p v-if="!posts?.length">Публикаций пока нет</p>
+            <div v-else class="blockList">
+                <div class="post" v-for="post of posts" :key="post.id">
+                <NuxtImg :src="`${post.img}`" sizes="300px"/>
+                <h2><NuxtLink :to="`/posts/${post.id}_${post.title_en}`">{{ post.title }}</NuxtLink></h2>
+                <p>{{ post.preview }}</p>
                 </div>
             </div>
+        </div>
 
-        </section>
+    </section>
 
-    </div>
 
 </template>
 
@@ -149,4 +36,6 @@ useSeoMeta({
   twitterCard: 'summary_large_image',
 })
 
+const {data} = await useFetch('/api/post/by_rubrics/4')
+const posts = ref(data.value?.posts)
 </script>
