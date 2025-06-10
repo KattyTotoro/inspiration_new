63f1a437d1f37a9be29f00b5b7d548a0804472cc
KattyTotoro <141658513+KattyTotoro@users.noreply.github.com>
2025-06-10 21:08:58 +0300

    postgres fix

--- server/routes/update/git.get.ts
+++ server/routes/update/git.get.ts
@@ -2,7 +2,7 @@ import { exec } from 'child_process'
 
 
 export default defineEventHandler(async (event) => {
-    exec('git pull && npm i && cp default node_modules/@prisma/client/default.js && npm run build && pm2 restart 0', (error, stdout, stderr) => {
+    exec('git pull && npm i && cp default node_modules/@prisma/client/default.js && npm run build && pm2 restart app', (error, stdout, stderr) => {
     if (error) {
         console.error(`Error: ${error}`)
     }
