94d0b934711f79d31c967f2e754043deb37bcd47
KattyTotoro <141658513+KattyTotoro@users.noreply.github.com>
2025-06-19 20:40:28 +0300

    prisma fix 3

--- server/routes/update/git.get.ts
+++ server/routes/update/git.get.ts
@@ -2,7 +2,7 @@ import { exec } from 'child_process'
 
 
 export default defineEventHandler(async (event) => {
-    exec('git pull && npm i && cp default node_modules/@prisma/client/default.js && npm run build && pm2 restart app', (error, stdout, stderr) => {
+    exec('git pull && npm i && cp default node_modules/@prisma/client/default.js && cp browser node_modules/@prisma/client/index-browser.js && npm run build && pm2 restart app', (error, stdout, stderr) => {
     if (error) {
         console.error(`Error: ${error}`)
     }
