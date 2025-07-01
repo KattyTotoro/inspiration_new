85682468eb4ee4c4bd048151dee26fbe6565c856
KattyTotoro <141658513+KattyTotoro@users.noreply.github.com>
2025-07-01 20:49:22 +0300

    nuxt-image fix2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,7 +49,8 @@ export default defineNuxtConfig({
     ],
     alias: {
       inspirationworld: 'https://inspirationworld.ru'
-    }
+    },
+    dir: 'public'
   },
   prisma: {
     generateClient: true,
