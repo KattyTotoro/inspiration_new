b62e5b2409ec7478ba2bb07c07fed0c9ae3df056
KattyTotoro <89184036854@yandex.ru>
2025-08-19 19:46:43 +0300

    metrika fix2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,13 +23,13 @@ export default defineNuxtConfig({
           {
             type:'text/javascript',
             innerHTML:`
-      (function(m,e,t,r,i,k,a){
-          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();
-          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
-      ym(103808775, 'init', {ssr:true, webvisor:true, clickmap:true, accurateTrackBounce:true, trackLinks:true});
+      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),k.async=1,k.src=r,e.head.appendChild(k)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+      window.ym(103808775, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+      });
           `}
         ],
       },
