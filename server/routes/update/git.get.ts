db399673c952bfa5f5d109ec1c068be2a1bf40f9
KattyTotoro <89184036854@yandex.ru>
2025-10-26 21:08:41 +0300

    delivery update fix

--- server/routes/update/git.get.ts
+++ server/routes/update/git.get.ts
@@ -2,7 +2,7 @@ import { exec } from 'child_process'
 
 
 export default defineEventHandler(async (event) => {
-  exec('git pull && npm i && npx prisma db push && cp default node_modules/@prisma/client/default.js && cp browser node_modules/@prisma/client/index-browser.js && npm run build && pm2 restart app', (error, stdout, stderr) => {
+  exec('git pull && npm i && npx prisma db push && cp default node_modules/@prisma/client/default.js && cp browser node_modules/@prisma/client/index-browser.js && npm run build && pm2 restart app && pm2 restart 1', (error, stdout, stderr) => {
     if (error) {
       console.error(`Error: ${error}`)
     }
