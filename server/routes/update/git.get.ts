7f5d47f7faacc833737f553c6388771fd992765b
KattyTotoro <89184036854@yandex.ru>
2025-10-26 21:06:34 +0300

    delivery update

--- server/routes/update/git.get.ts
+++ server/routes/update/git.get.ts
@@ -2,15 +2,15 @@ import { exec } from 'child_process'
 
 
 export default defineEventHandler(async (event) => {
-    exec('git pull && npm i --force && cp default node_modules/@prisma/client/default.js && cp browser node_modules/@prisma/client/index-browser.js && npm run build && pm2 restart app', (error, stdout, stderr) => {
+  exec('git pull && npm i && npx prisma db push && cp default node_modules/@prisma/client/default.js && cp browser node_modules/@prisma/client/index-browser.js && npm run build && pm2 restart app', (error, stdout, stderr) => {
     if (error) {
-        console.error(`Error: ${error}`)
+      console.error(`Error: ${error}`)
     }
     if (stderr) {
       console.error(`Stderr: ${stderr}`)
     }
     console.log(`Stdout: ${stdout}`)
-    }) 
-    // git pull && npm run build
-    return {ok:true}
+  })
+  // git pull && npm run build
+  return { ok: true }
 })
