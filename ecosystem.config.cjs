f5a3496875dd3b36492389e5905d1941e3708638
KattyTotoro <89184036854@yandex.ru>
2026-03-01 19:19:48 +0300

    fix

---
+++ ecosystem.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  apps: [{
+    name: "app",
+    script: ".output/server/index.mjs",
+    env: {
+      DATABASE_URL: "postgresql://postgres:parol@localhost:5432/inspirationworld",
+      NODE_ENV: 'production',
+    },
+  }]
+}
